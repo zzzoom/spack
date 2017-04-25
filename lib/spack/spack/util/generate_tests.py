2d60d991d833c924159c8eff7cc73de47185ea59
kiel friedt <friedt2@llnl.gov>
2017-04-25 13:48:53 -0700

    fixed bug

--- lib/spack/spack/util/generate_tests.py
+++ lib/spack/spack/util/generate_tests.py
@@ -31,7 +31,6 @@ class GenerateTests(object):
 
         self.cdash = "https://spack.io/cdash/submit.php?project=spack"
         self.seperate_by_compiler = seperate_by_compiler
-
         methods = {'all-tests': self.generate_all_tests,
                    'xsdk': self.generate_xsdk,
                    'days': self.generate_days}
@@ -41,24 +40,37 @@ class GenerateTests(object):
             raise Exception("Method %s not implemented" % specific_test_type)
 
     def seperate_compilers(self, complr_list):
-        rtn_dict = {}
-        for cmpr in complr_list:
-            cmpr = str(cmpr)
-            cmplr_name = cmpr.split('@')[0]
-            cmplr_version = cmpr.split('@')[1]
-            if cmplr_name not in rtn_dict.keys():
-                rtn_dict[cmplr_name] = {'versions': []}
-                rtn_dict[cmplr_name]['versions'].append(cmplr_version)
-            else:
-                rtn_dict[cmplr_name]['versions'].append(cmplr_version)
+        rtn_dict = {'compilers': {}}
+        for cmplr in complr_list:
+            cmplr = str(cmplr)
+            cmplr_name = cmplr.split('@')[0]
+            cmplr_version = cmplr.split('@')[1]
+            if cmplr_name not in rtn_dict['compilers'].keys():
+                rtn_dict['compilers'][cmplr_name] = {'versions': []}
+            rtn_dict['compilers'][cmplr_name]['versions'].append(cmplr_version)
         return rtn_dict
+
+    def file_output(self, pkgs, compilers):
+        file_dict = {}
+        file_dict["test-suite"] = {}
+        file_dict["test-suite"]["include"] = pkgs
+        file_dict[
+            "test-suite"]["matrix"] = self.generate_packages(pkgs, compilers)
+        file_dict["test-suite"]["cdash"] = [self.cdash]
+        return file_dict
+
+    def write_file(self, path, pkgs, compilers):
+        with open(path, 'w') as f:
+            sys.stdout = f
+            print yaml.dump(self.file_output(pkgs, compilers),
+                            default_flow_style=False)
 
     def generate_all_tests(self):
         self.generate_days()
         self.generate_all()
         self.generate_xsdk()
 
-    def generate_packages(self, compiler,  pkg_list):
+    def generate_packages(self,  pkg_list, compilers):
         pkgs = {}
         rm_list = []
         num_pkgs = len(pkg_list)
@@ -72,38 +84,20 @@ class GenerateTests(object):
         for rm_pkg in rm_list:
             tmp_pkg_list.remove(rm_pkg)
         num_pkgs = num_pkgs - len(rm_list)
-        if self.seperate_by_compiler:
-            pkg_cmplrs = [{"packages": pkgs},
-                          self.seperate_compilers(self.compilers)]
-        else:
-            pkg_cmplrs = [{"packages": pkgs},
-                          self.seperate_compilers([compiler])]
+        pkg_cmplrs = [{"packages": pkgs},
+                      self.seperate_compilers(compilers)]
         return pkg_cmplrs
-
-    def file_output(self, pkgs, compiler):
-        file_dict = {}
-        file_dict["test-suite"] = {}
-        file_dict["test-suite"]["include"] = pkgs
-        file_dict[
-            "test-suite"]["matrix"] = self.generate_packages(compiler, pkgs)
-        file_dict["test-suite"]["cdash"] = [self.cdash]
-        return file_dict
 
     def generate_days(self):
         # days 1-7
         all_pkgs = spack.repo.all_package_names()
-        for compiler in self.compilers:
-            for day in range(1, 8):
-                tmp_pkg_list = list(all_pkgs)
-                num_pkgs = len(tmp_pkg_list)
-                if self.seperate_by_compiler:
+        for day in range(1, 8):
+            tmp_pkg_list = list(all_pkgs)
+            num_pkgs = len(tmp_pkg_list)
+            if self.seperate_by_compiler:
+                for compiler in self.compilers:
                     path = os.path.join(os.getcwd(), "day" +
                                         str(day) + "_" + str(compiler) + ".yaml")
-                else:
-                    path = os.path.join(os.getcwd(), "day" +
-                                        str(day) + ".yaml")
-                with open(path, 'w') as f:
-                    sys.stdout = f
                     rm_list = []
                     day_list = []
                     for x in range(num_pkgs / 7):
@@ -112,31 +106,38 @@ class GenerateTests(object):
                             item = tmp_pkg_list[randint(0, num_pkgs - 1)]
                         rm_list.append(item)
                         day_list.append(item)
-                    print yaml.dump(self.file_output(day_list, compiler),
-                                    default_flow_style=False)
+                        self.write_file(path, day_list, [str(compiler)])
+            else:
+                rm_list = []
+                day_list = []
+                for x in range(num_pkgs / 7):
+                    item = tmp_pkg_list[randint(0, num_pkgs - 1)]
+                    while item in rm_list:
+                        item = tmp_pkg_list[randint(0, num_pkgs - 1)]
+                    rm_list.append(item)
+                    day_list.append(item)
+                    path = os.path.join(os.getcwd(), "day" +
+                                        str(day) + ".yaml")
+                    self.write_file(path, day_list, self.compilers)
 
     def generate_all(self):
         all_pkgs = list(spack.repo.all_package_names())
-        for compiler in self.compilers:
-            if self.seperate_by_compiler:
+        if self.seperate_by_compiler:
+            for compiler in self.compilers:
                 path = os.path.join(os.getcwd(), "all_" +
                                     str(compiler) + ".yaml")
-            else:
-                path = os.path.join(os.getcwd(), "all.yaml")
-            with open(path, 'w') as f:
-                sys.stdout = f
-                print yaml.dump(self.file_output(all_pkgs, compiler),
-                                default_flow_style=False)
+                self.write_file(path, all_pkgs, [str(compiler)])
+        else:
+            path = os.path.join(os.getcwd(), "all.yaml")
+            self.write_file(path, all_pkgs, self.compilers)
 
     def generate_xsdk(self):
         xsdk_pkgs = self.xsdk
-        for compiler in self.compilers:
-            if self.seperate_by_compiler:
+        if self.seperate_by_compiler:
+            for compiler in self.compilers:
                 path = os.path.join(os.getcwd(), "xsdk_" +
                                     str(compiler) + ".yaml")
-            else:
-                path = os.path.join(os.getcwd(), "xsdk.yaml")
-            with open(path, 'w') as f:
-                sys.stdout = f
-                print yaml.dump(self.file_output(xsdk_pkgs, compiler),
-                                default_flow_style=False)
+                self.write_file(path, self.xsdk, [str(compiler)])
+        else:
+            path = os.path.join(os.getcwd(), "xsdk.yaml")
+            self.write_file(path, self.xsdk, self.compilers)
