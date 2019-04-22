a93ddd216eafa2b23d23880cc86b9ae057f42c35
Todd Gamblin <tgamblin@llnl.gov>
2019-04-22 13:54:24 +0900

    py-flake8: Change py-flake8 hashes to sha256

--- var/spack/repos/builtin/packages/py-flake8/package.py
+++ var/spack/repos/builtin/packages/py-flake8/package.py
@@ -13,10 +13,18 @@ class PyFlake8(PythonPackage):
     homepage = "https://github.com/PyCQA/flake8"
     url      = "https://github.com/PyCQA/flake8/archive/3.0.4.tar.gz"
 
-    version('3.7.7',  sha256='b3f76b02351008dc772276e74b09dd3d4b5c567ff8c6ab573352cb8fd7007444')
-    version('3.5.0', '4e312803bbd8e4a1e566ffac887ae647')
-    version('3.0.4', 'cf2a7d8c92070f7b62253404ffb54df7')
-    version('2.5.4', '366dd1de6c300254c830b81e66979f06')
+    version('3.7.7', sha256='b3f76b02351008dc772276e74b09dd3d4b5c567ff8c6ab573352cb8fd7007444')
+    version('3.7.6', sha256='2729990a3ec78ed17a2b6177bed2b18c6bfd152ca8ec20fff500a1e36ba761b4')
+    version('3.7.5', sha256='64014a82b5d81d30393b505b71ec6457c7e8f2761d78bc1b27921bc9f0842580')
+    version('3.7.4', sha256='eb27064b99d9f7d11a7e79c4265383d12fd912c44c605bc6a295d2fd7f5d4075')
+    version('3.7.3', sha256='42af44b5da9fd5a4bd6f602f57e68b9f8f412aefcae3efc304ab4f862fbd24de')
+    version('3.7.2', sha256='27f26794d0468383e186d387b41c677126c48416b9d2e1f354803843ca28a91f')
+    version('3.7.1', sha256='b5da9b8cb1f54741c733150c6a898daffe9ef9d0373cf4dc7c082f39042d6931')
+    version('3.7.0', sha256='9da2aa8e642a7d442ccf0a2594887a65f2079079d8b2c70f38fcaaf7c6e40a4e')
+    version('3.6.0', sha256='c82a8b59c99e9c7862231b5739e8b1ceef6303cc1199bf508de654ad7a482ea8')
+    version('3.5.0', sha256='60ffe2fdacce4ebe7cadc30f310cf1edfd8ff654ef79525d90cf0756e69de44e')
+    version('3.0.4', sha256='87a2b642900a569fc2f27ab3b79573e0d02d2fee7445c6abab84eb33dcb60365')
+    version('2.5.4', sha256='ce03cc1acbe1726775ca57b40fab1d177550debb2f2f6b7a3c860541f3971cf5')
 
     extends('python', ignore='bin/(pyflakes|pycodestyle)')
     depends_on('python@2.7:2.8,3.4:')
