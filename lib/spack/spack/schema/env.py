8c6f71bc3ef3c5707fa80e0b415da7f5060ab9c3
Gregory Becker <becker33@llnl.gov>
2020-02-27 13:43:29 -0800

    add testing

--- lib/spack/spack/schema/env.py
+++ lib/spack/spack/schema/env.py
@@ -75,8 +75,10 @@ schema = {
                         'patternProperties': {
                             r'\w[\w-]*': {
                                 'type': 'object',
+                                'required': ['source', 'version'],
+                                'additionalProperties': False,
                                 'properties': {
-                                    'source_path': {
+                                    'source': {
                                         'type': 'string',
                                     },
                                     'version': {
