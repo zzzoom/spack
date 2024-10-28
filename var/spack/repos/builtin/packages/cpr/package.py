fac92dcecaeee326d18dfd08335b2b51d4471d64
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2024-10-28 08:39:16 +0100

    cpr: add versions up to v1.11 (#47242)

--- var/spack/repos/builtin/packages/cpr/package.py
+++ var/spack/repos/builtin/packages/cpr/package.py
@@ -12,8 +12,11 @@ class Cpr(CMakePackage):
     homepage = "https://docs.libcpr.org/"
     url = "https://github.com/libcpr/cpr/archive/refs/tags/1.10.4.tar.gz"
 
+    maintainers("prudhomm")
     license("MIT")
 
+    version("1.11.0", sha256="fdafa3e3a87448b5ddbd9c7a16e7276a78f28bbe84a3fc6edcfef85eca977784")
+    version("1.10.5", sha256="c8590568996cea918d7cf7ec6845d954b9b95ab2c4980b365f582a665dea08d8")
     version("1.10.4", sha256="88462d059cd3df22c4d39ae04483ed50dfd2c808b3effddb65ac3b9aa60b542d")
     version("1.9.2", sha256="3bfbffb22c51f322780d10d3ca8f79424190d7ac4b5ad6ad896de08dbd06bf31")
 
