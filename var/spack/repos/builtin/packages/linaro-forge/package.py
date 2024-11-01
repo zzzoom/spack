9a25a5821920c75bd4b85eb4332b1e4c4798e267
joscot-linaro <126488600+joscot-linaro@users.noreply.github.com>
2024-11-01 03:56:02 +0000

    linaro-forge: added 24.0.6 version (#47348)

--- var/spack/repos/builtin/packages/linaro-forge/package.py
+++ var/spack/repos/builtin/packages/linaro-forge/package.py
@@ -24,6 +24,9 @@ class LinaroForge(Package):
 
     if platform.machine() == "aarch64":
         version(
+            "24.0.6", sha256="a7f9f71e4352be3680854611fe433a9974fcb8a327ac65ca3bc950c956eac6e4"
+        )
+        version(
             "24.0.5", sha256="fc0c80ce9f66c6966faaca77de0f13e26da564c853e5bfc1e8acd17b65bc2ba0"
         )
         version(
@@ -97,6 +100,9 @@ class LinaroForge(Package):
             "21.1.3", sha256="eecbc5686d60994c5468b2d7cd37bebe5d9ac0ba37bd1f98fbfc69b071db541e"
         )
     elif platform.machine() == "x86_64":
+        version(
+            "24.0.6", sha256="eab198b964862b4664359ccbec1edb27c2dd3b9fa82bcb4e14fc616a2b0341da"
+        )
         version(
             "24.0.5", sha256="da0d4d6fa9120b5e7c4a248795b7f5da32c4987588ecb7406213c8c9846af2bc"
         )
