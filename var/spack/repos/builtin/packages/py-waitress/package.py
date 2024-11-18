cf672ea8afe2de31896609357d16517730e3c352
Wouter Deconinck <wdconinc@gmail.com>
2024-11-18 14:50:49 -0600

    py-waitress: add v3.0.1 (#47509)

--- var/spack/repos/builtin/packages/py-waitress/package.py
+++ var/spack/repos/builtin/packages/py-waitress/package.py
@@ -14,6 +14,11 @@ class PyWaitress(PythonPackage):
 
     license("ZPL-2.1")
 
-    version("2.1.2", sha256="780a4082c5fbc0fde6a2fcfe5e26e6efc1e8f425730863c04085769781f51eba")
+    version("3.0.1", sha256="ef0c1f020d9f12a515c4ec65c07920a702613afcad1dbfdc3bcec256b6c072b3")
+    with default_args(deprecated=True):
+        # https://nvd.nist.gov/vuln/detail/CVE-2024-49769
+        version("2.1.2", sha256="780a4082c5fbc0fde6a2fcfe5e26e6efc1e8f425730863c04085769781f51eba")
+
+    depends_on("python@3.8:", type=("build", "run"), when="@3:")
 
     depends_on("py-setuptools@41:", type="build")
