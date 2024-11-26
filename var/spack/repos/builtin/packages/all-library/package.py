cd306d0bc6b8b4a895f3c9b83efdfc1e6dc47e9b
Christoph Junghans <junghans@lanl.gov>
2024-11-26 14:56:22 -0700

    all-libary: add voronoi support and git version (#47798)
    * all-libary: add voronoi support and git version
    
    ---------
    
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/all-library/package.py
+++ var/spack/repos/builtin/packages/all-library/package.py
@@ -15,21 +15,30 @@ class AllLibrary(CMakePackage):
 
     homepage = "http://slms.pages.jsc.fz-juelich.de/websites/all-website/"
     url = "https://gitlab.jsc.fz-juelich.de/SLMS/loadbalancing/-/archive/v0.9.2/loadbalancing-v0.9.2.tar.gz"
+    git = "https://gitlab.jsc.fz-juelich.de/SLMS/loadbalancing.git"
 
     maintainers("junghans")
 
     license("BSD-3-Clause", checked_by="junghans")
 
+    version("master", branch="master")
     version("0.9.2", sha256="2b4ef52c604c3c0c467712d0912a33c82177610b67edc14df1e034779c6ddb71")
 
     variant("fortran", default=False, description="Build with fortran support")
     variant("shared", default=True, description="Build shared libraries")
     variant("vtk", default=False, description="Build with vtk support")
+    variant(
+        "voronoi",
+        default=False,
+        description="Enable voronoi-based loadbalancing scheme",
+        when="@0.9.3:",
+    )
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("fortran", type="build", when="+fortran")
     depends_on("vtk", when="+vtk")
+    depends_on("voropp", when="+voronoi")
 
     depends_on("mpi")
 
@@ -39,6 +48,7 @@ class AllLibrary(CMakePackage):
             self.define_from_variant("CM_ALL_FORTRAN", "fortran"),
             self.define_from_variant("CM_ALL_USE_F08", "fortran"),
             self.define_from_variant("CM_ALL_VTK_OUTPUT", "vtk"),
+            self.define_from_variant("CM_ALL_VORONOI", "voronoi"),
         ]
 
         if self.run_tests:
