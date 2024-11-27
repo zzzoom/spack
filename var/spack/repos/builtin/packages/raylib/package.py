8a6428746fafb602c5f6447d25d024473e298daf
George Malerbo <89956051+georgemalerbo@users.noreply.github.com>
2024-11-27 18:25:22 -0500

    raylib: add v5.5 (#47708)
    * Add version 5.5 in package.py
    
    * Update package.py

--- var/spack/repos/builtin/packages/raylib/package.py
+++ var/spack/repos/builtin/packages/raylib/package.py
@@ -17,6 +17,7 @@ class Raylib(CMakePackage):
 
     license("Zlib", checked_by="georgemalerbo")
 
+    version("5.5", sha256="aea98ecf5bc5c5e0b789a76de0083a21a70457050ea4cc2aec7566935f5e258e")
     version("5.0", sha256="98f049b9ea2a9c40a14e4e543eeea1a7ec3090ebdcd329c4ca2cf98bc9793482")
 
     depends_on("c", type="build")  # generated
