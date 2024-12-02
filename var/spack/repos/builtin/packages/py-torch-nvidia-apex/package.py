f4d81be9cf88db00d5ef36907d2fb8ca23c4b610
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-12-02 20:37:33 +0100

    py-torch-nvidia-apex: add dependency on C (#47868)

--- var/spack/repos/builtin/packages/py-torch-nvidia-apex/package.py
+++ var/spack/repos/builtin/packages/py-torch-nvidia-apex/package.py
@@ -26,6 +26,7 @@ class PyTorchNvidiaApex(PythonPackage, CudaPackage):
     version("22.03", tag="22.03")
     version("2020-10-19", commit="8a1ed9e8d35dfad26fb973996319965e4224dcdd")
 
+    depends_on("c", type="build")
     depends_on("cxx", type="build")
 
     variant("cuda", default=True, description="Build with CUDA")
